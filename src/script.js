e6e68682744722d32fcb732bcc84fbfb89f1285f
shopnilashik <shopnil.ashik@gmail.com>
2022-06-13 14:38:51 +0600

    new changes

--- src/script.js
+++ src/script.js
@@ -6,6 +6,10 @@ const priceValue = document.getElementById("p_price");
 const categoryValue = document.getElementById("p_category");
 var pname = document.getElementById("productNameValidation");
 var x = document.getElementById("p_name");
+const image = document.getElementById("image");
+let imageName = "";
+let allUser = [];
+let u_id = "";
 // Update value
 const output = document.querySelector("#output");
 const btnSubmit = document.querySelector("#btnSubmit");
@@ -17,7 +21,10 @@ const title = document.getElementById("title");
 const productNameValidation = document.getElementById("productNameValidation");
 const modal_container = document.getElementById("modal-container");
 const close = document.getElementById("close");
-
+const popup_delete = document.getElementById("popup-modal-delete");
+const dclose = document.getElementById("dclose");
+const btnDclose = document.getElementById("btnDclose");
+const confirmBtn = document.getElementById("confirmBtn");
 loadCategory();
 showdata();
 open.addEventListener("click", () => {
@@ -96,8 +103,9 @@ const renderUser = (users) => {
                 <td class="border border-slate-600" id="detail_id" >${user.p_details}</td>
                 <td class="border border-slate-600" id="price_id" >${user.p_price}</td>
                 <td class="border border-slate-600" id="category_id" >${user.category_name}</td>
-                <td class="p-2 border border-slate-600 text-center"><a id="id_delete">Delete</a></td>
-                <td class="p-2 border border-slate-600 text-center"><a id="id_edit">Update</a></td>
+                <td class="border border-slate-600" id="category_id" ><img class="h-[100px] w-[100px]" src="img/${user.image}"></td>
+                <td class="p-2 border border-slate-600 text-center"><a data-id=${user.id} id="id_delete" onclick="deleteUser(${user.id})">Delete</a></td>
+                <td class="p-2 border border-slate-600 text-center"><a id="id_edit_${user.id}" onclick="editUser(${user.id})">Update</a></td>
                 
             </tr>
             
@@ -118,108 +126,123 @@ function showdata() {
   modal_container.classList.remove("show");
   fetch("http://localhost/sct/api/products/read.php")
     .then((response) => response.json())
-    .then((data) => renderUser(data));
-}
-
-//DELETE OPATARION
-tableList.addEventListener("click", (e) => {
-  e.preventDefault();
-  let onPressDelete = e.target.id == "id_delete";
-  let onPressedit = e.target.id == "id_edit";
-  let id = e.target.parentElement.parentElement.dataset.id;
-  const parent = e.target.parentElement.parentElement;
-  if (onPressDelete) {
-    var result = confirm("Want to delete?");
-    if (result) {
-      fetch("http://localhost/sct/api/products/delete.php", {
-        method: "DELETE",
-        body: JSON.stringify({
-          id: id,
-        }),
-      })
-        .then(() => showdata())
-        .then((res) => res.json());
-    }
-  }
-  if (onPressedit) {
-    const modal_container = document.getElementById("modal-container");
-    modal_container.classList.add("show");
-    title.innerHTML = "Update Product";
-
-    const close = document.getElementById("close");
-    close.addEventListener("click", () => {
-      modal_container.classList.remove("show");
+    .then((data) => {
+      allUser = [...data];
+      renderUser(data);
     });
-
-    let cid = e.target.parentElement.parentElement.dataset.cid;
-    const p_name = parent.querySelector("#name_id").textContent;
-    const detail_id = parent.querySelector("#detail_id").textContent;
-    const price_id = parent.querySelector("#price_id").textContent;
-    // POPULATING DATA
-    p_nameValue.value = p_name;
-    detailValue.value = detail_id;
-    priceValue.value = price_id;
-    categoryValue.value = cid;
-    console.log(cid);
-    console.log(p_nameValue.value);
-    console.log(detailValue.value);
-    console.log(p_price.value);
-  }
-  //UPDATE OPARATION
-  btnSubmit.addEventListener("click", (e) => {
-    e.preventDefault();
-    fetch("http://localhost/sct/api/products/update.php", {
-      method: "PUT",
-      headers: {
-        "Content-type": "application/json",
-      },
+}
+function removePopup() {
+  popup_delete.classList.remove("show");
+}
+function deleteUser(id) {
+  popup_delete.classList.add("show");
+  dclose.addEventListener("click", () => {
+    removePopup();
+  });
+  btnDclose.addEventListener("click", () => removePopup());
+  confirmBtn.addEventListener("click", () => {
+    fetch("http://localhost/sct/api/products/delete.php", {
+      method: "DELETE",
       body: JSON.stringify({
         id: id,
-        p_name: p_nameValue.value,
-        p_details: detailValue.value,
-        p_category: p_category.value,
-        p_price: p_price.value,
       }),
     })
-      .then((res) => res.json())
       .then(() => showdata())
-      .then(() => {
-        id = "";
-      });
+      .then((res) => res.json());
+    removePopup();
   });
-});
+}
+function editUser(id) {
+  modal_container.classList.add("show");
+  title.innerHTML = "Update Product";
 
-//CREATE PRODUCT
-addForm.addEventListener("submit", (e) => {
+  const close = document.getElementById("close");
+  close.addEventListener("click", () => {
+    modal_container.classList.remove("show");
+  });
+
+  var user = allUser.find((x) => x.id == id);
+  u_id = user.id;
+  p_nameValue.value = user.p_name;
+  detailValue.value = user.p_details;
+  priceValue.value = user.p_price;
+  categoryValue.value = user.p_category;
+}
+// EDIT PRODUCT
+btnSubmit.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log(categoryValue.value);
-  if (isNaN(categoryValue.value)) {
-    pname.innerHTML = "Please Select a Category";
-  } else if (
-    p_nameValue.value.length > 4 &&
-    p_nameValue.value.length < 20 &&
-    categoryValue.value.length > 0
-  ) {
-    fetch("http://localhost/sct/api/products/create.php", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify({
-        ProductName: p_nameValue.value,
-        ProductDetails: detailValue.value,
-        categoriy_id: categoryValue.value,
-        ProductPrice: p_price.value,
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        const dataArr = [];
-        dataArr.push(data);
-        renderUser(dataArr);
-      })
-      .then(() => showdata());
+  if (u_id > 0) {
+    // fetch("http://localhost/sct/api/products/update.php", {
+    //   method: "PUT",
+    //   headers: {
+    //     "Content-type": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     id: u_id,
+    //     p_name: p_nameValue.value,
+    //     p_details: detailValue.value,
+    //     p_category: p_category.value,
+    //     p_price: p_price.value,
+    //   }),
+    // })
+    //   .then((res) => res.json())
+    //   .then(() => showdata())
+    //   .then(() => {
+    //     u_id = "";
+    //   });
   } else {
-    alert("Enter at least 4 charecter and not more than 20 characters");
+    // if (isNaN(categoryValue.value)) {
+    //   console.log(image.value);
+    //   pname.innerHTML = "Please Select a Category";
+    // } else if (p_nameValue.value.length > 4 && p_nameValue.value.length < 20) {
+    //   fetch("http://localhost/sct/api/products/create.php", {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-type": "application/json",
+    //     },
+    //     body: JSON.stringify({
+    //       ProductName: p_nameValue.value,
+    //       ProductDetails: detailValue.value,
+    //       categoriy_id: categoryValue.value,
+    //       ProductPrice: p_price.value,
+    //       image: imageName,
+    //     }),
+    //   })
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //       const dataArr = [];
+    //       dataArr.push(data);
+    //       renderUser(dataArr);
+    //     })
+    //     .then(() => showdata());
+    // } else {
+    //   pname.innerHTML = "Please Select a Category";
+    // }
   }
+});
+function upload(event) {
+  const value = event.target.value;
+  // this will return C:\fakepath\somefile.ext
+  // console.log(value);
+  const files = event.target.files;
+
+  //this will return an ARRAY of File object
+  console.log(files);
+  for (let file of files) {
+    imageName = file.name;
+    // console.log(file.size);
+  }
+}
+addForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log("Hello");
+  const formData = new FormData(addForm);
+});
+// get form element from the dom
+const newsletterForm = document.getElementById("newsletter-form");
+
+// handle signup on submit
+newsletterForm.addEventListener("submit", (event) => {
+  event.preventDefault(); // prevent page refresh
+  console.log("first");
 });
